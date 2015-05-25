67c56f4c846a68df3711fba9731e4c7cdc89ca10
coderchira <ruchira.aggarwal@gmail.com>
2015-05-25 17:36:23 +0530

    FormsExercise

---
+++ SecondAssignment/Form1.vb
@@ -0,0 +1,11 @@
+﻿'Search for products (By attributes)
+'Retrieve ProductList on click of button
+' Display in listbox 1 the items(Product names) that user searches for
+' If user clicks on items in listbox1; listbox2 should display the clicked items' attributes
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
